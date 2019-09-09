67639591d69035770cb9c3b484fd7c877e0de98e
okaufman <olivia.kaufm@gmail.com>
2019-09-09 17:30:33 +0200

    re-initialize the project

---
+++ Assets/drumscript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drumscript : MonoBehaviour
+{
+    public AudioSource drumSound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        drumSound = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("bam");
+        drumSound.Play();
+
+    }
+}
