5e283cf576ad6dd42904654d763c7b308930014c
Luca Rava <luca.rava@stud.hslu.ch>
2019-09-10 16:05:46 +0200

    set rotation for Sticks

--- Assets/Scripts/showStick.cs
+++ Assets/Scripts/showStick.cs
@@ -13,6 +13,8 @@ public class showStick : MonoBehaviour
     private bool availableSticks = false;
     private GameObject leftStick = null;
     private GameObject rightStick = null;
+    private Quaternion rotationLeftHand;
+    private Quaternion rotationRightHand;
 
     // Update is called once per frame
     void Update()
@@ -21,9 +23,13 @@ public class showStick : MonoBehaviour
         bool onScreen = screenPoint.x > 0.4 && screenPoint.x < 0.6 && screenPoint.z > 0.4 && screenPoint.z > 0.6;
         if(onScreen && !availableSticks)
         {
+            rotationLeftHand = leftHand.rotation;
+            rotationRightHand = rightHand.rotation;
+            rotationLeftHand.Set(0, 0, 90, 0);
+            rotationRightHand.Set(0, 0, 90, 0);
             availableSticks = true;
-            leftStick = Instantiate(stickType, -leftHand.position + new Vector3(0,0.1f,0), leftHand.rotation, leftHand);
-            rightStick = Instantiate(stickType, rightHand.position + new Vector3(0, 0.1f, 0), rightHand.rotation, rightHand);
+            leftStick = Instantiate(stickType, leftHand.position + new Vector3(0,0.1f,0), rotationLeftHand, leftHand);
+            rightStick = Instantiate(stickType, rightHand.position + new Vector3(0, 0.1f, 0), rotationRightHand, rightHand);
         }
         else if(!onScreen)
         {
