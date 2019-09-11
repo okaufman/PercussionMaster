0d9b3156195b1ea988f07a9a0ec6fe3dc85d1c61
Luca Rava <luca.rava@stud.hslu.ch>
2019-09-11 13:27:55 +0200

    add variable from new prefab to Scripts

--- Assets/Scripts/showStick.cs
+++ Assets/Scripts/showStick.cs
@@ -10,6 +10,8 @@ public class showStick : MonoBehaviour
     public Transform leftHand;
     public Transform rightHand;
 
+    public Transform leftPalm, rightPalm, leftFingerEnd, rightFingerEnd;
+
     private bool availableSticks = false;
     private GameObject leftStick = null;
     private GameObject rightStick = null;
@@ -25,18 +27,34 @@ public class showStick : MonoBehaviour
     {
         Vector3 screenPoint = cameraView.WorldToViewportPoint(targetPoint.position);
         bool onScreen = screenPoint.x > 0.4 && screenPoint.x < 0.6 && screenPoint.z > 0.4 && screenPoint.z > 0.6;
-        if(onScreen && !availableSticks)
+        if(onScreen && !availableSticks && isHandClosed())
         {
             availableSticks = true;
             leftStick = Instantiate(stickType, leftHand,false);
             rightStick = Instantiate(stickType, rightHand, false);
             transformLefStick = leftStick.GetComponent<Transform>();
         }
-        else if(!onScreen)
+        else if(!onScreen || !isHandClosed())
         {
             availableSticks = false;
             Destroy(leftStick);
             Destroy(rightStick);
         }
+    }
+
+    bool isHandClosed()
+    {
+        bool closedHand = false;
+
+        float leftHandDistanz = (leftPalm.position - leftFingerEnd.position).sqrMagnitude;
+        float rightHandDistanz = (rightPalm.position - rightFingerEnd.position).sqrMagnitude;
+
+        Debug.Log("Left: " + leftHandDistanz);
+        Debug.Log("Right: " + rightHandDistanz);
+
+        if (leftHandDistanz < 0.02 && rightHandDistanz < 0.02)
+            closedHand = true;
+
+        return closedHand;
     }
 }
