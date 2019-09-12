8c4536f519ad196f244dbd6bf2e1f46466f858f4
Olivia Kaufmann <olivia.kaufmann@ilub.unibe.ch>
2019-09-12 11:11:04 +0200

    add CSound when sticks collide

---
+++ Assets/prefabs/Schlaeger/stickCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickCollision : MonoBehaviour
+{
+
+    public AudioSource stickSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioSource>().playOnAwake = false;
+        stickSound = GetComponent<AudioSource>();
+    }
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision hit");
+        stickSound.Play();
+
+    }
+}
